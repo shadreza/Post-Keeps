f99700107d8cbc7160c13352ce1ca999dd020152
Shad Reza <shadreza100@gmail.com>
2021-08-22 13:35:37 +0600

    Here the landing page is done with the toggling optoin done in the app.js file... maybe the color may need to be changed but thats a topic for after...

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import LandingPage from './Components/Pages/LandingPage';
 import {Color} from './Utilities/Colors/Color';
+import {Sizes} from './Utilities/Sizes/Sizes';
 
 export default function App() {
+
+  const [onLandingPageOrNot, setOnLandingPageOrNot] = useState(true)
+
   return (
     <View style={styles.container}>
+      {
+        onLandingPageOrNot === true ?
+          <LandingPage toggler={[onLandingPageOrNot, setOnLandingPageOrNot]}/>
+          :
+          <Text>Here there is gonna be a homepage</Text>
+      }
       
       <StatusBar style="auto" />
     </View>
@@ -16,8 +27,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: Color.teal,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingTop: 40,
+    paddingTop: Sizes.xxxlg,
+    paddingBottom: Sizes.sm,
+    paddingLeft: Sizes.sm,
+    paddingRight: Sizes.sm,
   },
 });
