1000552b4e269da9ec122d262d98160196912ef8
Shad Reza <shadreza100@gmail.com>
2021-08-24 04:58:07 +0600

    have to implement the firebase and firestore

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+// @refresh
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
@@ -6,6 +7,23 @@ import LoginPage from './Components/Pages/LoginPage';
 import SignupPage from './Components/Pages/SignupPage';
 import {Color} from './Utilities/Colors/Color';
 import {Sizes} from './Utilities/Sizes/Sizes';
+
+// import * as firebase from 'firebase'
+// import 'firebase/firestore'
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC035-l-qMqkFD60_QRYqS3fADgJV8gbKA",
+//   authDomain: "post-keeper.firebaseapp.com",
+//   projectId: "post-keeper",
+//   storageBucket: "post-keeper.appspot.com",
+//   messagingSenderId: "550139856924",
+//   appId: "1:550139856924:web:d6c95e7a5879ff3b8a77d9"
+// };
+
+// if (firebase.apps.length === 0) {
+//   firebase.initializeApp(firebaseConfig)
+// }
+
 
 export default function App() {
 
